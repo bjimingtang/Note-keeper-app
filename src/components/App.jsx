fd37632265cd85e6fedda04eadc60ed757e7648e
bjimingtang <brendan.jiming.tang@gmail.com>
2021-04-26 23:07:22 -0700

    comments added for reference

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,17 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  // initialize notes as an array
   const [notes, setNotes] = useState([]);
   function addNote(note) {
+    // append note to array
     setNotes(prevNotes => {
       return [...prevNotes, note];
     });
   };
   function deleteNote(id) {
     setNotes(prevNotes => {
+      // filter out the node with the matching id
       return prevNotes.filter((note, index) => {
         return index !== id;
       });
@@ -22,6 +25,7 @@ function App() {
     <div>
       <Header />
       <CreateArea submitFunction={addNote}/>
+      // map the notes, creating a Note component displaying each
       {notes.map((note, index) => {
         return <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote}/>
       })}
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -4,18 +4,23 @@ import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  // give the component a variable to determine whether or not to expand the area
   const [isExpanded, setExpanded] = useState(false);
   function expand(event) {
+    // toggle expansion
     setExpanded(prevExpanded => {
       return !prevExpanded;
     });
   };
+  // hold the user input as a note to submit when the user submits
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
   function handleChange(event) {
+    // pick up the name and value of the edited field
     const {name, value} = event.target;
+    // change the chosen field to the chosen value
     setNote(prevNote => {
       return {...prevNote,[name]: value};
     });
@@ -31,6 +36,7 @@ function CreateArea(props) {
   return (
     <div>
       <form className="create-note">
+        // display the title field if expanded
         {isExpanded && <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />}
         <textarea
           name="content"
@@ -40,6 +46,7 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows={isExpanded ? 3 : 1}
         />
+        // submit button if expanded
         {isExpanded && <Zoom in={true}>
           <Fab onClick={submitNote}>
             <AddIcon />
